566137e0908a4783e152e77e43dfa8ca51e4289e
Oicanji <51388779+Oicanji@users.noreply.github.com>
2023-03-01 21:47:36 -0300

    DIA 6 - remover
    ultima função

--- flask-2/Notas/app/views.py
+++ flask-2/Notas/app/views.py
@@ -2,6 +2,8 @@ from flask import Blueprint
 from flask import render_template
 from flask_login import login_required, current_user
 from flask import request, render_template, url_for, redirect, flash
+from flask import jsonify
+import json
 from app import db
 from .models import Note
 
@@ -20,3 +22,15 @@ def home():
             flash('Nota adicionada!', category='success')
 
     return render_template("pages/home.html", user=current_user)
+
+@views.route('/delete-note', methods=['POST'])
+def delete_note():
+    note = json.loads(request.data)
+    noteId = note['noteId']
+    note = Note.query.get(noteId)
+    if note:
+        if note.user_id == current_user.id:
+            db.session.delete(note)
+            db.session.commit()
+
+    return jsonify({})

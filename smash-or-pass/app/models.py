b7e088912be79aea430833099225afea660968dd
unknown <ignaciopunto102@outlook.com>
2023-03-05 21:55:30 -0300

    Logica do Javascript
    Toda a logica foi adicionada, junto com um modo depuração

--- smash-or-pass/app/models.py
+++ smash-or-pass/app/models.py
@@ -7,11 +7,44 @@ class User(db.Model, UserMixin):
     email = db.Column(db.String(100), unique=True)
     password = db.Column(db.String(100))
     first_name = db.Column(db.String(100))
-    quizes = db.relationship('quiz')
+    type = db.Column(db.String(100))
 
-
-class Note(db.Model):
+class Quiz(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    note = db.Column(db.String(10000))
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    name = db.Column(db.String(100))
+    description = db.Column(db.String(10000))
     date = db.Column(db.DateTime(timezone=True), default=db.func.now())
+    create_by = db.Column(db.Integer, db.ForeignKey('User.id'))
+    categories = db.relationship('ListCategories')
+
+class ListCategories(db.Model):
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    quiz_id = db.Column(db.Integer, db.ForeignKey('Quiz.id'))
+    categories_id = db.Column(db.Integer, db.ForeignKey('Categories.id'))
+
+class Categories(db.Model):
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.String(100))
+    color = db.Column(db.String(100))
+
+class Char(db.Model):
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.String(100))
+    image = db.Column(db.String(500))
+    quiz_id = db.Column(db.Integer, db.ForeignKey('Quiz.id'))
+    statistics = db.relationship('Statistics')
+
+class Statistics(db.Model):
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    type = db.Column(db.String(100))
+    value = db.Column(db.Integer)
+
+class StatisticsResult(db.Model):
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    char_id = db.Column(db.Integer, db.ForeignKey('Char.id'))
+
+class CharRespost(db.Model):
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    quiz_id = db.Column(db.Integer, db.ForeignKey('Quiz.id'))
+    char_id = db.Column(db.Integer, db.ForeignKey('Char.id'))
+    user_id = db.Column(db.Integer, db.ForeignKey('User.id'))

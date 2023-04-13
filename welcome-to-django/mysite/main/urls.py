34f3ee05dd62e6bcc00bd74e262a5175d3106896
Oicanji <51388779+Oicanji@users.noreply.github.com>
2023-04-12 21:32:51 -0300

    AULA 11

--- welcome-to-django/mysite/main/urls.py
+++ welcome-to-django/mysite/main/urls.py
@@ -2,10 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home'),
+    path('', views.index, name='home'),
     path('todolist/<int:id>', views.getToDoList, name='todolist'),
     path('itemstatuschange/<int:id>', views.itemStatusChange, name='itemstatuschange'),
-    path('createnewlist', views.createNewList, name='createnewlist'),
+    path('createnewlist/', views.createNewList, name='createnewlist'),
     path('createnewitem/<int:id>', views.createNewItem, name='createnewitem'),
     path('getitems/<int:id>', views.getItems, name='getitems'),
     path('delitem/<int:id>', views.delItem, name='delitem'),
--- welcome-to-django/mysite/main/views.py
+++ welcome-to-django/mysite/main/views.py
@@ -6,12 +6,14 @@ import json
 from .models import  *
 from .forms import *
 
-def index(request):
-    return HttpResponse("<h1>WELCOME TO DJANGO!!</h1>")
+from django.contrib.auth.models import User
+from django.contrib.auth.decorators import login_required
 
-def home(request):
+@login_required
+def index(request):
     return render(request, 'main/home.html', {})
 
+@login_required
 def getToDoList(request, id = 0):
     if id == 0:
         return render(request, 'main/home.html', {})
@@ -22,8 +24,12 @@ def getToDoList(request, id = 0):
 
     todo = ToDoList.objects.get(id=id)
 
+    if todo.user != request.user:
+        return render(request, 'main/home.html', {})
+
     return render(request, 'main/todolist.html', {"todo": todo})
 
+@login_required
 def itemStatusChange(request, id):
     print(request.POST)
     if request.method == "POST":
@@ -37,19 +43,25 @@ def itemStatusChange(request, id):
     else:
         return HttpResponse("404")
 
+@login_required
 def createNewList(response):
     if response.method == "POST":
         form = CreateNewList(response.POST)
         if form.is_valid():
             name = form.cleaned_data["name"]
-            t = ToDoList(name=name)
+            t = ToDoList(name=name, user=response.user)
             t.save()
+            response.user.todolist.add(t)
         return HttpResponseRedirect('/todolist/%i' % t.id)
     else:
         form = CreateNewList()
     return render(response, 'main/create.html', {"form": form})
 
+@login_required
 def createNewItem(response, id):
+    if not response.user.is_authenticated:
+        return HttpResponseRedirect('/login/')
+    
     if response.method == "POST":
         text = response.POST.get("form")
         if not text:
@@ -64,7 +76,11 @@ def createNewItem(response, id):
     else:
         return HttpResponse("404")
     
+@login_required
 def getItems(request, id):
+    if not request.user.is_authenticated:
+        return HttpResponseRedirect('/login/')
+    
     if not ToDoList.objects.filter(id=id).exists():
         return HttpResponse("404")
     items = Item.objects.filter(todolist=id)
@@ -73,8 +89,11 @@ def getItems(request, id):
     json_format = json.dumps([{"id": item.id, "text": item.text, "complete": item.complete} for item in items])
     return HttpResponse(json_format)
 
+@login_required
 def delItem(request, id):
-    print(request.POST)
+    if not request.user.is_authenticated:
+        return HttpResponseRedirect('/login/')
+    
     id = request.POST.get("id")
     if not Item.objects.filter(id=id).exists():
         return HttpResponse("404")
